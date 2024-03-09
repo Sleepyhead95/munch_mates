1a7cf3405f4cd5cfbad36cb02704e0ca86c1f8ae
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-03-09 12:38:33 +0100

    styling search bar

--- src/components/header/SearchBar.js
+++ src/components/header/SearchBar.js
@@ -3,13 +3,24 @@ import styles from "./searchBar.module.scss";
 export default function SearchBar() {
   return (
     <div className={styles.search_bar}>
-      <input
-        className={styles.text}
-        type="text"
-        placeholder="Search"
-        name="search"
-      />
-      <input className={styles.btn} type="submit" name="search-submit" />
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <img src="./icons/search.svg" alt="search" />
+          <input
+            className={styles.text}
+            type="text"
+            placeholder="Search"
+            name="search"
+          />
+        </div>
+        <input
+          className={styles.btn}
+          type="submit"
+          name="search-submit"
+          aria-label="Search"
+          value="Go"
+        />
+      </div>
     </div>
   );
 }
