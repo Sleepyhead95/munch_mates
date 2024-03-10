10123ff39a3d68fcf3295604ae99cf5744e2a11a
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-03-10 16:28:10 +0100

    implemented show/hide scroll animation for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,35 @@
 import styles from "./header.module.scss";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import React, { useState, useEffect, useCallback } from "react";
 
 export default function Header() {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = useCallback(() => {
+    if (window.scrollY > lastScrollY) {
+      // if scroll down, hide the navbar
+      setShow(false);
+    } else {
+      // if scroll up, show the navbar
+      setShow(true);
+    }
+    // remember current page location to use in the next move
+    setLastScrollY(window.scrollY);
+  }, [lastScrollY]); // Dependencies for useCallback
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [controlNavbar]);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${!show ? styles.hidden : ""}`}>
       <div className={styles.logo}>
         <a href="/home">
           <img src="MM_logo.png" alt="MunchMates logo" />
